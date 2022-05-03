1edb4e3b806e9483c62cd3ca85ca5eb800750387
Thomas Camminady <92997216+ThomasCamminady@users.noreply.github.com>
2022-05-03 08:47:25 +0200

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -23,22 +23,38 @@ def print_futher_instuctions():
     """Shows user what to do next after project creation."""
     message = f"""
     
-    Your project {PROJECT_NAME} is created.
-    Create an empty repository at: git@github.com:{ORGANIZATION}/{PROJECT_NAME}.git
     
-    To install all packages, initializes git, add all files, 
-    and push to git@github.com:{ORGANIZATION}/{PROJECT_NAME}.git, 
-    just run:
+                    
+    
+    #######################################################################################
+    #                                                                                     #
+    #                 __          __     _    _  ____   ____                              #
+    #                 \ \        / /\   | |  | |/ __ \ / __ \                             #
+    #                  \ \  /\  / /  \  | |__| | |  | | |  | |                            #
+    #                   \ \/  \/ / /\ \ |  __  | |  | | |  | |                            #
+    #                    \  /\  / ____ \| |  | | |__| | |__| |                            #
+    #                     \/  \/_/    \_\_|  |_|\____/ \____/                             #
+    #                                                                                     #
+    #                                                                                     #
+    #    Your project {PROJECT_NAME} is created.                                          #
+    #    Create an empty repository at: git@github.com:{ORGANIZATION}/{PROJECT_NAME}.git  #
+    #                                                                                     #
+    #    To install all packages, initializes git, add all files,                         #
+    #    and push to git@github.com:{ORGANIZATION}/{PROJECT_NAME}.git,                    #
+    #    just run:                                                                        #
+    #                                                                                     #
+    #                                                                                     #
+    #                                                                                     #
+    #                                                                                     #
+    #                                                                                     #
+                                  
+                                  cd {PROJECT_NAME}
+                                  git  init 
+                                  poetry install
+                                  make gitsetup
+                                  make gitfirst
+    
 
-
-        cd {PROJECT_NAME}
-        git  init 
-        poetry install
-        poetry shell
-        make gitsetup
-        make gitfirst
-        
-   
     """
     print(message)  # noqa: WPS421
 
